269ee62afb4d5b620579af3ec9a62a478593b051
vonconrad <sebastian@e-channel.com.au>
2010-12-28 18:18:08 +1030

    Add mod column

--- db/migrate/20101228074046_create_mod_components.rb
+++ db/migrate/20101228074046_create_mod_components.rb
@@ -1,6 +1,7 @@
 class CreateModComponents < ActiveRecord::Migration
   def self.up
     create_table :mod_components do |t|
+      t.references :mod
       t.integer :number
       t.string :label
       t.timestamps
