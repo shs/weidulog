c678435127c824b4574a9ac38ea7e84b0a300e2b
vonconrad <sebastian@e-channel.com.au>
2010-12-28 18:46:34 +1030

    Add all resource actions

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,2 +1,70 @@
 class LogsController < ApplicationController
+  def index
+    @logs = Log.all
+
+    respond_to do |format|
+      format.html
+      format.xml  { render :xml => @logs }
+    end
+  end
+
+  def show
+    @logs = Log.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @log }
+    end
+  end
+
+  def new
+    @log = Log.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @log }
+    end
+  end
+
+  def edit
+    @log = Log.find(params[:id])
+  end
+
+  def create
+    @log = Log.new(params[:log])
+
+    respond_to do |format|
+      if @log.save
+        format.html { redirect_to(@log, :notice => 'Log was successfully created.') }
+        format.xml  { render :xml => @log, :status => :created, :location => @log }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @log.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    @log = Log.find(params[:id])
+
+    respond_to do |format|
+      if @log.update_attributes(params[:log])
+        format.html { redirect_to(@log, :notice => 'Log was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @log.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @log = Log.find(params[:id])
+    @log.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(logs_url) }
+      format.xml  { head :ok }
+    end
+  end
 end
