83cbcdcd21754945fdba4f3293b401520d10f11b
vonconrad <sebastian@e-channel.com.au>
2010-12-28 17:58:37 +1030

    Rails structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weidulog::Application.config.secret_token = 'f628489e8c386a133971cf471a05223c343cec27605912d13e41e4164483874e48e639884a4a4e449a5f0bf6427f9499740866645395a991b8ac99926394a417'
