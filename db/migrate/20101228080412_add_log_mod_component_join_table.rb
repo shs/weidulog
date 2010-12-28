6efbb0a409345a7e5aba28f4bf85c6adc0d8b3ee
vonconrad <sebastian@e-channel.com.au>
2010-12-28 18:34:40 +1030

    Add join table for logs and mod components

---
+++ db/migrate/20101228080412_add_log_mod_component_join_table.rb
@@ -0,0 +1,11 @@
+class AddLogModComponentJoinTable < ActiveRecord::Migration
+  def self.up
+    create_table :logs_mod_components, :id => false do |t|
+      t.references :log, :mod
+    end
+  end
+
+  def self.down
+    drop_table :logs_mod_components
+  end
+end
