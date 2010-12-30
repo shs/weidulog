bd28e87574bec660091f6a9749832e24c35b72a3
vonconrad <sebastian@e-channel.com.au>
2010-12-30 16:19:12 +1030

    Make sure to include params in ordering links

--- app/helpers/logs_helper.rb
+++ app/helpers/logs_helper.rb
@@ -6,6 +6,6 @@ module LogsHelper
 
     direction  = is_order && ascending ? 'desc' : 'asc'
     link_text  = is_order ? (ascending ? "#{title} ^" : "#{title} v") : title
-    link_to link_text, {:order_by => column, :order_direction => direction}
+    link_to link_text, {:order_by => column, :order_direction => direction, :filters => params[:filters]}
   end
 end
