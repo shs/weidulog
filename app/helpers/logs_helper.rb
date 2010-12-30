0e7f6adaac897cf72f0007649edcf6363d7a7c53
vonconrad <sebastian@e-channel.com.au>
2010-12-30 16:14:48 +1030

    Add sorting links

--- app/helpers/logs_helper.rb
+++ app/helpers/logs_helper.rb
@@ -1,2 +1,11 @@
 module LogsHelper
+  def link_to_order(column, title=nil)
+    title   ||= column.titleize
+    is_order  = params[:order_by] == column
+    ascending = params[:order_direction] == 'asc'
+
+    direction  = is_order && ascending ? 'desc' : 'asc'
+    link_text  = is_order ? (ascending ? "#{title} ^" : "#{title} v") : title
+    link_to link_text, {:order_by => column, :order_direction => direction}
+  end
 end
