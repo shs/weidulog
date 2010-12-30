42f60488dd8859c48dcbce4db3f29b0ff5bea4ee
vonconrad <sebastian@e-channel.com.au>
2010-12-30 18:01:59 +1030

    Guess mod name on create

--- app/models/mod.rb
+++ app/models/mod.rb
@@ -2,4 +2,11 @@ class Mod < ActiveRecord::Base
   has_many :components
   has_many :versions
   has_and_belongs_to_many :logs
+
+  before_create :guess_name
+
+  private
+    def guess_name
+      self.name = tp2_name.titleize.gsub('Bg', 'BG')
+    end
 end
