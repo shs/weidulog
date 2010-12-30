2fc141d8e6a6e3ed821ef908b168927ba9b4bc6e
vonconrad <sebastian@e-channel.com.au>
2010-12-30 14:14:29 +1030

    Add parsing of log and saving of mods/components

--- app/models/log.rb
+++ app/models/log.rb
@@ -5,4 +5,25 @@ class Log < ActiveRecord::Base
   has_and_belongs_to_many :mod_components
 
   accepts_nested_attributes_for :content
+
+  after_save :parse_mods
+
+  private
+    def parse_mods
+      mods.clear
+
+      content.body.each_line do |line|
+        matches = line.match(/^~(?:(?:.+?)\/)?(?:SETUP-)?(.+?)\.TP2~\s#\d+\s#(\d+)\s\/\/\s(.+?)(?::\sv?([\d\.]+)\s?)?$/)
+
+        if matches
+          mod_name, component_number, component_label, mod_version = matches[1..4]
+
+          mod       = Mod.find_or_create_by_name_and_version(mod_name, mod_version)
+          component = mod.components.find_or_create_by_number_and_label(component_number, component_label)
+
+          mods           << mod       unless mod_ids.include?(mod.id)
+          mod_components << component unless mod_component_ids.include?(component.id)
+        end
+      end
+    end
 end
