46ae8573d703667e8d969482d58d6d3fcb2c0cc4
vonconrad <sebastian@e-channel.com.au>
2010-12-30 16:45:00 +1030

    Move link_to_order to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def link_to_order(column, title=nil)
+    title   ||= column.to_s.titleize
+    is_order  = params[:order_by] == column.to_s
+    ascending = params[:order_direction] == 'asc'
+    direction = is_order && ascending ? 'desc' : 'asc'
+    link_text = is_order ? (ascending ? "#{title} ^" : "#{title} v") : title
+
+    link_to link_text, {:order_by => column, :order_direction => direction, :filters => params[:filters]}
+  end
 end
