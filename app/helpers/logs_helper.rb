ca811f1abb81fcb6d504870b303cdb2687275a69
vonconrad <sebastian@e-channel.com.au>
2010-12-30 16:35:22 +1030

    Make columns symbols instead of strings

--- app/helpers/logs_helper.rb
+++ app/helpers/logs_helper.rb
@@ -1,7 +1,7 @@
 module LogsHelper
   def link_to_order(column, title=nil)
-    title   ||= column.titleize
-    is_order  = params[:order_by] == column
+    title   ||= column.to_s.titleize
+    is_order  = params[:order_by] == column.to_s
     ascending = params[:order_direction] == 'asc'
 
     direction  = is_order && ascending ? 'desc' : 'asc'
