cd5c9cfa0001c336d4bda206c0ee6ec311966e3d
vonconrad <sebastian@e-channel.com.au>
2011-01-08 18:21:05 +1030

    Rename file field to :file instead of :body

--- app/models/log_content.rb
+++ app/models/log_content.rb
@@ -4,16 +4,18 @@ class LogContent < ActiveRecord::Base
   before_validation :read_log
   validate :valid_log
 
+  attr_accessor :file
+
   def regex
     /^~(?:(?:.+?)\/)?(?:SETUP-)?(.+?)\.TP2~\s#\d+\s#(\d+)\s\/\/\s(.+?)(?::\sv?([\d\.]+)\s?)?$/
   end
 
   private
     def read_log
-      self.body = body.read
+      self.body = file.read
     end
 
     def valid_log
-      errors.add(body, 'is not a valid WeiDU log') unless body.match(regex)
+      errors.add(:file, 'is not a valid WeiDU log') unless body.match(regex)
     end
 end
