00055ffaef57befe4e1e69d8a006d15782963500
vonconrad <sebastian@e-channel.com.au>
2011-01-08 17:35:36 +1030

    Add validation for log content

--- app/models/log_content.rb
+++ app/models/log_content.rb
@@ -1,7 +1,8 @@
 class LogContent < ActiveRecord::Base
   belongs_to :log
 
-  before_create :read_log
+  before_validation :read_log
+  validate :valid_log
 
   def regex
     /^~(?:(?:.+?)\/)?(?:SETUP-)?(.+?)\.TP2~\s#\d+\s#(\d+)\s\/\/\s(.+?)(?::\sv?([\d\.]+)\s?)?$/
@@ -10,5 +11,9 @@ class LogContent < ActiveRecord::Base
   private
     def read_log
       self.body = body.read
+    end
+
+    def valid_log
+      errors.add(body, 'is not a valid WeiDU log') unless body.match(regex)
     end
 end
